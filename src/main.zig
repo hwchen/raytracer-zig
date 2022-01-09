075c8cfd63d19e49aa4daf524b4ae999d2d6833e
Walther Chen <walther.chen@gmail.com>
2022-01-09 11:17:51 -0500

    try initial render w/ SDL

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,13 @@ const c = @cImport({
 
 const SDL_WINDOWPOS_UNDEFINED = @bitCast(c_int, c.SDL_WINDOWPOS_UNDEFINED_MASK);
 
-const window_width: c_int = 640;
-const window_height: c_int = 420;
+const window_width: c_int = 256;
+const window_height: c_int = 256;
 
 pub fn main() !void {
+
     // SDL Setup
+
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
@@ -20,15 +22,35 @@ pub fn main() !void {
         c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
+    defer c.SDL_DestroyWindow(window);
 
-    const surface = c.SDL_GetWindowSurface(window) orelse {
+    // Does this need to be destroyed also?
+    var surface = c.SDL_GetWindowSurface(window) orelse {
         c.SDL_Log("Unable to get Window surface: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
 
-    _ = surface;
+    // render
+
+    setPixel(surface, 2, 2, 0);
+    //var w: c_int = 0;
+    //var h: c_int = 0;
+    //while (w < window_width) : (w += 1) {
+    //    while (h < window_height) : (h += 1) {
+    //        const r = @intToFloat(f32, w) / @intToFloat(f32, window_width - 1);
+    //        const g = @intToFloat(f32, h) / @intToFloat(f32, window_height - 1);
+    //        const b = 0.25;
+    //        const pixel = rgbToPixel(
+    //            @floatToInt(u32, 255.99 * r),
+    //            @floatToInt(u32, 255.99 * g),
+    //            @floatToInt(u32, 255.99 * b),
+    //        );
+    //        setPixel(surface, w, h, pixel);
+    //    }
+    //}
 
     // Keep window open until receiving quit event
+
     var running = true;
     while (running) {
         var event: c.SDL_Event = undefined;
@@ -42,4 +64,19 @@ pub fn main() !void {
         }
         c.SDL_Delay(16);
     }
+}
+
+fn setPixel(surf: *c.SDL_Surface, x: c_int, y: c_int, pixel: u32) void {
+    // looks like calculating pointer offset
+    // - pitch is length of a row of pixels in bytes
+    // - x is multiplied times 4 because a pixel is 4 bytes
+    const target_pixel = @ptrToInt(surf.pixels) +
+        @intCast(usize, y) * @intCast(usize, surf.pitch) +
+        @intCast(usize, x) * 4;
+    @intToPtr(*u32, target_pixel).* = pixel;
+}
+
+// TODO figure out how rgb maps to the u32
+fn rgbToPixel(r: u32, g: u32, b: u32) u32 {
+    return 255 << 24 | r << 16 | g << 8 | b;
 }
